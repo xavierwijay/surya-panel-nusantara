b03e642a6de4fba5711b5d89fd5e0ee6d2291c0d
Xavier Wijaya <xavierpro14@gmail.com>
2026-02-02 22:33:53 +0700

    feat: Add Hero section and configure Unsplash image domains in Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.panelsuryasolo.com", // Domain produksi
+  image: {
+    domains: ["images.unsplash.com", "plus.unsplash.com"],
+  },
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
